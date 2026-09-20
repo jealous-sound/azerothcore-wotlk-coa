1e225867b25d61377c641338ede80aa85bc16227
jealous-sound <325820559+jealous-sound@users.noreply.github.com>
2026-09-20 13:13:39 +0300

    fix(Primalist): restore Mountain Fury pulls and scaling (#632)

--- modules/mod-ascension-compat/src/MP_loader.cpp
+++ modules/mod-ascension-compat/src/MP_loader.cpp
@@ -133,6 +133,7 @@ void AddSC_AscensionPrimalistSeismicResources();
 void AddSC_AscensionPrimalistGuidance();
 void AddSC_AscensionPrimalistBarrier();
 void AddSC_AscensionPrimalistGrove();
+void AddSC_AscensionPrimalistMountainFury();
 void AddSC_AscensionRunemasterTalents();
 void AddSC_AscensionRunemasterTravel();
 void AddSC_AscensionStormbringerTalents();
@@ -299,6 +300,7 @@ void Addmod_ascension_compatScripts()
     AddSC_AscensionPrimalistGuidance();
     AddSC_AscensionPrimalistBarrier();
     AddSC_AscensionPrimalistGrove();
+    AddSC_AscensionPrimalistMountainFury();
     AddSC_AscensionRunemasterTalents();
     AddSC_AscensionRunemasterTravel();
     AddSC_AscensionStormbringerTalents();
