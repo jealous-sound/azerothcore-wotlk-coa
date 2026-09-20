3354472cbc4c847c693d474436f26228f2e35481
jealous-sound <325820559+jealous-sound@users.noreply.github.com>
2026-09-20 07:30:55 +0300

    fix(CoA/Primalist): trigger Bring Me Their Bones on the fifth pet hit (#457)

--- modules/mod-ascension-compat/src/MP_loader.cpp
+++ modules/mod-ascension-compat/src/MP_loader.cpp
@@ -127,6 +127,7 @@ void AddSC_AscensionVenomancerCatalyst();
 void AddSC_AscensionPrimalistSpiritBeast();
 void AddSC_AscensionPrimalistWeapons();
 void AddSC_AscensionPrimalistTalents();
+void AddSC_AscensionPrimalistBones();
 void AddSC_AscensionRunemasterTalents();
 void AddSC_AscensionRunemasterTravel();
 void AddSC_AscensionStormbringerTalents();
@@ -287,6 +288,7 @@ void Addmod_ascension_compatScripts()
     AddSC_AscensionPrimalistSpiritBeast();
     AddSC_AscensionPrimalistWeapons();
     AddSC_AscensionPrimalistTalents();
+    AddSC_AscensionPrimalistBones();
     AddSC_AscensionRunemasterTalents();
     AddSC_AscensionRunemasterTravel();
     AddSC_AscensionStormbringerTalents();
